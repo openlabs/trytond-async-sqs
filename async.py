7cf603f3bf9c3092e4381aeded8370f0e25207f8
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2014-12-14 09:59:19 +0530

    Add support for instance methods

--- async.py
+++ async.py
@@ -12,6 +12,8 @@
     :license: 3-clause BSD License, see COPYRIGHT for more details
 """
 import time
+import inspect
+import logging
 import functools
 from uuid import uuid4
 from collections import namedtuple
@@ -20,12 +22,14 @@ from datetime import datetime, timedelta
 import boto
 from trytond.config import CONFIG
 from trytond.pool import PoolMeta, Pool
-from trytond.model import ModelView
+from trytond.model import ModelView, Model
 from trytond.transaction import Transaction
 from .serialization import json, JSONDecoder, JSONEncoder
 
 
 __metaclass__ = PoolMeta
+
+logger = logging.getLogger('AsyncSQS')
 
 
 if 'sqs_region' in CONFIG.options:
@@ -45,10 +49,30 @@ def async_task(model_name=None, ignore_result=True, visibility_timeout=60):
 
         def defer(*args, **kwargs):
             Async = Pool().get('async.async')
+
+            # Active record instance for cases where the method is called
+            # on an active record instance
+            instance = None
+
+            # Find if its an instance method from inspect
+            arg_spec = inspect.getargspec(function)
+
+            if 'self' in arg_spec.args:
+                model_name = args[0].__class__.__name__
+                instance, args = args[0], args[1:]
+            # elif 'cls' in arg_spec.args:
+            #    model_name = args[0].__name__
+            elif wrapped_func.model_name is None:
+                raise Exception('model_name is not defined')
+            else:
+                model_name = wrapped_func.model_name
+
             return Async.defer(
-                wrapped_func.model_name,
-                function.__name__,
-                args, kwargs,
+                model=model_name,
+                method=function.__name__,
+                instance=instance,
+                args=args,
+                kwargs=kwargs,
                 result_options=ResultOptions(ignore_result, visibility_timeout)
             )
 
@@ -161,32 +185,34 @@ class Async(ModelView):
         result = cls.execute(
             payload['model_name'],
             payload['method_name'],
+            payload['instance'],
             payload['args'],
             payload['kwargs'],
         )
 
         if not result_options.ignore_result:
-            # Make the queue first
-            queue = cls.get_queue(payload['__result_uuid__'], create=True)
-            queue.set_timeout(result_options.visibility_timeout)
-
             # Send the result as message
-            cls.send_to_sqs(queue, {'result': result})
+            cls.reply_to_sqs(payload['__result_uuid__'], {'result': result})
 
         return result
 
     @classmethod
-    def execute(cls, model, method, args, kwargs):
+    def execute(cls, model, method, instance, args, kwargs):
         """
         Execute the given task and return the result of the execution.
         """
+        if instance:
+            return getattr(instance, method)(*args, **kwargs)
         return getattr(Pool().get(model), method)(*args, **kwargs)
 
     @classmethod
-    def defer(cls, model, method, args=None, kwargs=None,
+    def defer(cls, method, model=None, instance=None,
+              args=None, kwargs=None,
               delay_seconds=0, attributes=None, result_options=None):
         """Wrapper for painless asynchronous dispatch of method
         inside given model.
+
+        Either model or instance must be specified.
 
         .. note::
 
@@ -197,24 +223,32 @@ class Async(ModelView):
         :param model: String representing global name of the model or
                       reference to model class itself.
         :param method: Name or method object
+        :param instance: The instance on which the method call should happen
+                         if it is an instance
         :param args: positional arguments passed on to method as list/tuple.
         :param kwargs: keyword arguments passed on to method as dict.
         :returns :class:`AsyncResult`:
         """
-        if isinstance(model, basestring):
-            model_name = model
-        else:
-            model_name = model.__name__
-
         if isinstance(method, basestring):
             method_name = method
         else:
             method_name = method.__name__
 
+        if isinstance(model, basestring):
+            model_name = model
+        elif model:
+            model_name = model.__name__
+        else:
+            model_name = None
+
+        if isinstance(instance, Model):
+            model_name = instance.__name__
+
         payload = {
             'database_name': Transaction().cursor.database_name,
             'user': Transaction().user,
             'model_name': model_name,
+            'instance': instance,
             'method_name': method_name,
             'args': args or [],
             'kwargs': kwargs or {},
@@ -285,6 +319,23 @@ class Async(ModelView):
         )
 
         return cls._result_class(payload)
+
+    @classmethod
+    def reply_to_sqs(cls, result_uuid, payload):
+        """
+        Send the given payload to the queue as a result.
+
+        :param payload: The dictionary of the message to send
+        :param delay_seconds: Number of seconds (0 - 900) to delay this message
+                              from being processed.
+        :param attributes: Message attributes to set.
+        """
+        connection = cls.get_sqs_connection()
+        queue = cls.get_queue(result_uuid, create=True)
+        return connection.send_message(
+            queue,
+            cls.serialize_payload(payload),
+        )
 
     @classmethod
     def get_json_encoder(cls):
